2d9ee82c8901e6d88c11e9db52947609bb7721a6
Ronald Arias <ariasron@hotmail.com>
2022-05-11 10:56:18 -0500

    installs eslint and configure it

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders hello world link', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/hello world/i)
+  expect(linkElement).toBeInTheDocument()
+})
